d64284e2584a1947ed986ed95e0b411c4bdf5eb0
Martin Milani <martin.milani.mdq@gmail.com>
2020-12-16 20:18:11 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        darkCyan: "#19a2ae",
+        darkblue: "#2d3248",
+        grayishBlue: "#6a6f81",
+        darkGrey: "#969696",
+      },
+      fontFamily: {
+        body: ["Kumbh Sans"],
+      },
+      backgroundImage: (theme) => ({
+        "card-pattern": "url('/public/images/bg-pattern-card.svg')",
+      }),
+      minWidth: {
+        0: "0",
+        min: "375px",
+        max: "1440",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
